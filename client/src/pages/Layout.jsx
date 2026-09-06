573578ede2084e4fa242e5e9a47b96c970dafd68
anil-156 <sawantanil0612@gmail.com>
2026-09-06 14:18:48 +0530

    error changes made in layout.jsx

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Navigate, Outlet } from 'react-router-dom'
-import SideBar from '../components/SideBar'
+import Sidebar from '../components/Sidebar'
 import { useAuth } from '../context/AuthContext'
 import Loading from '../components/Loading'
 
@@ -11,7 +11,7 @@ const Layout = () => {
   
   return (
     <div className="flex h-screen bg-linear-to-br from-slate-50 via-white to-indigo-50/30">
-      <SideBar />
+      <Sidebar />
       <main className="flex-1 overflow-y-auto">
         <div className="p-4 pt-16 sm:p-6 sm:pt-6 lg:p-8 max-w-400 mx-auto">
           <Outlet />
